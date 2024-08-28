76f35ae6b0da2f5709b2d3a5ef3a40c656571b8b
YourLocalDuck <prince.reuben@gmail.com>
2024-08-28 16:11:00 -0400

    JWT stored. Moved backendURL

--- frontend-p2/frontend/src/Interfaces/UserInterface.tsx
+++ frontend-p2/frontend/src/Interfaces/UserInterface.tsx
@@ -2,11 +2,13 @@ import { UUID } from "crypto";
 
 // Player and Team Manager Model
 export interface UserInterface {
-    userId?: UUID,
+    userId?: UUID | string,
     username: string,
-    password: string,
-    firstName: string,
-    lastName: string,
+    password?: string,
+    firstName?: string,
+    lastName?: string,
     role: string,
-    jwt?: string
+    jwt?: string,
+    teamName?: string,
+    salary?: number,
 }
