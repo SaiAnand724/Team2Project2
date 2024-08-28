9e28639723c67d87164df395010d0be07f762466
Jalen Martin <113473123+martinj19@users.noreply.github.com>
2024-08-28 16:34:50 -0400

    added some more functionality

--- frontend-p2/frontend/src/Interfaces/TeamInviteInterface.tsx
+++ frontend-p2/frontend/src/Interfaces/TeamInviteInterface.tsx
@@ -1,5 +1,7 @@
+import { UUID } from "crypto";
+
 export interface TeamInviteProposal {
-    proposalId?: number,
+    proposalId?: UUID,
     status: string,
     teamName: string,
     receiverUsername: string,
